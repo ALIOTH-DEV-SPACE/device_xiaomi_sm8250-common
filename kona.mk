101e8260795b8a248f223fa282e358afc1be256c
Ramii Ahmed <ramy@ahmedramy.com>
2021-06-17 08:44:25 +0000

    sm8250-common: import QTI vibrator HAL
    Change-Id: I03254bee9089713ea29ab258cac152120272b2ea

--- kona.mk
+++ kona.mk
@@ -407,8 +407,8 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-    
+    vendor.qti.hardware.vibrator.service.xiaomi_kona
+
 #VNDK
 PRODUCT_PACKAGES += \
     libgui_vendor \
